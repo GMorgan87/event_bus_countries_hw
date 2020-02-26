52324cc5597543cd20f5894b30e488031e9ba517
roosap <roosa.paivansalo@gmail.com>
2020-02-26 15:20:41 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <country-select :countries='countries'></country-select>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountrySelect from './components/CountrySelect.vue';
+// import CountryDetails from './components/CountryDetails.vue';
+import {eventBus} from './main.js';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => this.countries = data)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
+  },
+  components: {
+    "country-select": CountrySelect,
+    // "country-details": CountryDetails
+  }
+}
+</script>
+
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="country-select">
+    <select class="countries" v-model="selectedCountry">
+      <option disabled value="">Choose country</option>
+      <option :value="index" v-for="(country, index) in this.countries">{{country.name}}</option>
+    </select>
+    <button @click="handleClick">View country details</button>
+  </div>
+</template>
+
+<script>
+
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'country-select',
+  props: ['countries'],
+  data() {
+    return {selectedCountry: null}
+  },
+  methods: {
+    handleClick() {
+      eventBus.$emit('country-selected', this.selectedCountry)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
